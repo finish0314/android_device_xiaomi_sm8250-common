ff7a56f85ebf2664d03f0a6b87a9e6d9db638b88
Flicker372 <flicker372@outlook.com>
2024-08-04 20:05:30 +0800

    sm8250-common: overlay: adjust default captive portal server for Chinese users
    * Form HyperOS
    
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- kona.mk
+++ kona.mk
@@ -281,6 +281,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    NetworkStackOverlayCN
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
