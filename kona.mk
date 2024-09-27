0193af965d17f974bbd9ae8109088abfa5ae41e7
¨raystef66¨ <s.vanbarel@gmail.com>
2024-09-27 19:42:52 +0200

    sm8250-common: drop offending entry * android.hardware.power-service.xiaomi-libperfmgr

--- kona.mk
+++ kona.mk
@@ -471,9 +471,6 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
