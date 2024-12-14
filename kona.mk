MurtazaKolachi/device_xiaomi_apollo

sm8250-common: Import QTI Codec2 service and configs
From munch

Change-Id: I17a56c596f15a1d5c23582c5c3a7028d3e676aab

kona.mk
@@ -243,6 +243,8 @@ PRODUCT_PACKAGES += \
 
 # Media configs
 PRODUCT_PACKAGES += \
+    media_codecs_c2.xml \
+    media_codecs_c2_performance.xml \
     media_codecs_kona.xml \
     media_codecs_performance_kona.xml
 

finish0314/android_device_xiaomi_sm8250-common

kona.mk
@@ -238,6 +238,8 @@ PRODUCT_PACKAGES += \
 
 # Media configs
 PRODUCT_PACKAGES += \
+    media_codecs_c2.xml \
+    media_codecs_performance_c2.xml \
     media_codecs_kona.xml \
     media_codecs_performance_kona.xml
 